ca882a74dcf002b82464d80fe079fe97d2bdea1a
romano6379 <romano6379@gmail.com>
2026-06-17 13:37:12 +0900

    AutoPilot: update data/site-config.js

--- data/site-config.js
+++ data/site-config.js
@@ -1,1 +1,7 @@
-window.siteConfig = { "siteName": "Smart Wallet Guide" };
+window.siteConfig = {
+  "siteName": "Smart Wallet Guide",
+  "primaryColor": "#4f46e5",
+  "bgMain": "#faf9f6",
+  "fontHeading": "'Plus Jakarta Sans'",
+  "layoutStyle": "modern"
+};
